e75e49cea80e3e1158859192952d27685245e8ee
Eduardo <eduardotortellidudu9@uniplaclages.edu.br>
2024-04-08 18:14:59 -0300

    Rodando

--- src/Triangulos.ConsoleApp/Program.cs
+++ src/Triangulos.ConsoleApp/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace Triangulos.ConsoleApp
+﻿namespace Triangulo
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("Digite o primeiro lado do triângulo: ");
+            int x = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("\nDigite o segundo lado do triângulo: ");
+            int y = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("\nDigite o terceiro lado do triângulo: ");
+            int z = int.Parse(Console.ReadLine());
+
+            Triangulo triangulo = new Triangulo(x, y, z);
+
+            if (triangulo.VerificarTriangulo())
+            {
+                Console.WriteLine($"\nO triângulo é {triangulo.ObterTipoTriangulo()}.");
+            }
+
+            else
+            {
+                Console.WriteLine("\nMedidas não encaixam para formar um triângulo, tente novamente");
+            }
         }
     }
 }
---
+++ src/Triangulos.ConsoleApp/Triangulo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangulo
+{
+    public class Triangulo
+    {
+        public int x;
+        public int y;
+        public int z;
+
+        public Triangulo(int x, int y, int z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public bool VerificarTriangulo()
+        {
+            if (x + y <= z) return false;
+            if (x + z <= y) return false;
+            if (y + z <= x) return false;
+            return true;
+        }
+
+        public string ObterTipoTriangulo()
+        {
+            if (x == y && y == z) return "equilátero";
+            if (x == y || x == z || z == y) return "iscosceles";
+            return "Triângulo escaleno";
+        }
+
+    }
+}
